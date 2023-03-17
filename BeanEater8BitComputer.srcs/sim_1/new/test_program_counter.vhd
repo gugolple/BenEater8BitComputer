e879ac07a922649bb7e84ff8160029f7f477c920
Gugolple <>
2023-03-17 16:24:57 -0400

    Added program_counter.

--- BeanEater8BitComputer.srcs/sim_1/new/test_program_counter.vhd
+++ BeanEater8BitComputer.srcs/sim_1/new/test_program_counter.vhd
@@ -2,9 +2,9 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 03/17/2023 10:17:00 AM
+-- Create Date: 03/17/2023 04:13:52 PM
 -- Design Name: 
--- Module Name: test_counter - Behavioral
+-- Module Name: test_program_counter - Behavioral
 -- Project Name: 
 -- Target Devices: 
 -- Tool Versions: 
@@ -22,35 +22,42 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
+use work.constants;
+use work.test_counter;
 
-entity test_counter is
+entity test_program_counter is
     Generic(
         bit_size : integer := 4;
         iterations : integer := 2
     );
-end test_counter;
+end test_program_counter;
 
-architecture Behavioral of test_counter is
-component counter is
+architecture Behavioral of test_program_counter is
+component program_counter is
     Generic(
-        bit_size : integer
+        bit_size : integer := constants.bit_width 
     );
     Port (
         clk : in std_logic;
+        set : in std_logic;
         reset : in std_logic;
+        in_counter : in std_logic_vector(bit_size -1 downto 0);
         out_counter : out std_logic_vector(bit_size -1 downto 0)
     );
 end component;
     constant max_val : integer := 2 ** bit_size;
-    signal clk, reset : std_logic;
+    signal clk, set, reset : std_logic;
+    signal in_counter : std_logic_vector(bit_size -1 downto 0);
     signal out_counter : std_logic_vector(bit_size -1 downto 0);
 begin
 -- The counter
-counter_instance : counter 
+program_counter_instance : program_counter 
     Generic map (bit_size => bit_size)
     Port map(
         clk => clk,
+        set => set,
         reset => reset,
+        in_counter => in_counter,
         out_counter => out_counter
     );
     
@@ -60,6 +67,9 @@ process is
 begin
     -- Loop a couple of iterations
     for num_loop_count in 0 to iterations loop
+        -- Initialize program counter specifics
+        set <= '0';
+        in_counter <= (others => '0');
         -- Initialize element by pulsing a clock.
         reset <= '1';
         clk <= '0';
@@ -100,8 +110,22 @@ begin
             clk <= '0';
             wait for constants.period;
         end loop;
+        
+        set <= '1';
+        -- Test full loop manual set
+        for i in 0 to max_val -1 loop
+            in_counter <= std_logic_vector(to_unsigned(i,bit_size));
+            clk <= '1';
+            wait for constants.period;
+            clk <= '0';
+            wait for constants.period;
+            assert (std_logic_vector(to_unsigned(i,bit_size)) = out_counter) 
+                report "Error in counter at cycle " 
+                & integer'image(i) & " value read "
+                & integer'image(to_integer(signed(out_counter)))
+                severity error;
+        end loop;
     end loop;
     wait;
 end process;
-
 end Behavioral;
---
+++ BeanEater8BitComputer.srcs/sources_1/new/program_counter.vhd
@@ -0,0 +1,58 @@
+----------------------------------------------------------------------------------
+-- Company: MyOwnBoredom
+-- Engineer: Gugolple
+-- 
+-- Create Date: 03/17/2023 04:05:47 PM
+-- Module Name: program_counter - Behavioral
+-- Project Name: BenEater8BitComputer
+-- Description: This module will be a program counter, increasing each rising 
+--     edge of the clock pulse, with reset and with the ability to set it to
+--     any desired value by input.
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+
+use work.constants;
+
+entity program_counter is
+    Generic(
+        bit_size : integer := constants.bit_width 
+    );
+    Port (
+        clk : in std_logic;
+        set : in std_logic;
+        reset : in std_logic;
+        in_counter : in std_logic_vector(bit_size -1 downto 0);
+        out_counter : out std_logic_vector(bit_size -1 downto 0)
+    );
+end program_counter;
+
+architecture Behavioral of program_counter is
+
+begin
+
+process(reset,clk) is
+    variable count : unsigned(bit_size -1 downto 0) := (others => '0');
+begin
+    if reset = '1' then
+        count := (others => '0');
+    elsif rising_edge(clk) then
+        if set = '1' then
+            count := unsigned(in_counter);
+        else
+            count := count + 1;
+        end if;
+    end if;
+    out_counter <= std_logic_vector(count);
+end process;
+
+
+end Behavioral;
